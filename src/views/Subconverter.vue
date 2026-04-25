f09eac13d4ff55599821c93d463e837ea178d448
rjzxaz <150208233+rjzxaz@users.noreply.github.com>
2026-04-25 12:31:03 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -349,13 +349,13 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
-          "账号后端1": "https://hfsubapi.121810.xyz",
-          "账号后端2": "https://hfsubapib.121810.xyz",
+          "站长后端1": "https://subapi.zbzb.eu.org",
+          "站长后端2": "https://hfsubapib.121810.xyz",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
         },
         backendOptions: [
-          { value: "https://hfsubapi.121810.xyz" },
+          { value: "https://subapi.zbzb.eu.org" },
           { value: "https://hfsubapib.121810.xyz" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
@@ -808,7 +808,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://hfsubapi.121810.xyz" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://subapi.zbzb.eu.org" : this.getUrlParam(),
         shortType: "https://v1.121810.xyz/short",
         remoteConfig: "https://raw.githubusercontent.com/gongaadd/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_self-built.ini",
         excludeRemarks: "",
